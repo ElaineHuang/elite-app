c345c002603bfeed0ab1dc35ecc02ae417e2810e
mavisliu689 <r03241304@ntu.edu.tw>
2017-07-30 00:57:14 +0800

    change key

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def init_db_app(app):
-    # app.config['MONGO_DBNAME'] = 'eb8c4f80-3ec6-428d-a4df-ce2011778d7c'
+    # app.config['MONGOALCHEMY_DATABASE'] = 'eb8c4f80-3ec6-428d-a4df-ce2011778d7c'
     # app.config['MONGO_URI'] = 'mongodb://d933c021-d8d3-402b-aad2-bb67e8d40601:iEGGAujhUObOBJyFeakrOt3T3@192.168.100.12:27017/eb8c4f80-3ec6-428d-a4df-ce2011778d7c'
 
     #localhost db for test
